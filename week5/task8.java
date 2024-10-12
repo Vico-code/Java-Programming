3744b25c7af63c73d21e9ffa44870fc19d29f767
Vico-code <2074273338@qq.com>
2024-10-12 11:24:25 +0800

    Delete week5 directory

--- week5/task8.java
+++
@@ -1,8 +0,0 @@
-package week5;
-
-public class task8 {
-    public static void main(String[] args) {
-        String message = "Welcome to Java";
-        System.out.println("The length of " + message + " is " + message.length());
-    }
-}
--- week5/task9.java
+++
@@ -1,8 +0,0 @@
-package week5;
-
-public class task9 {
-    public static void main(String[] args) {
-        String message = "Welcome to Java";
-        System.out.println("The first character in message is " + message.charAt(0));
-    }
-}
